b5360c046cfdf39919be0cdae6ea6b1317ddd99e
LeoSalgueiro <dismoi.leo@gmail.com>
2025-03-08 22:57:24 -0300

    Added double linked list (contains prev)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
+import { DoubleLinkedList } from './linkedlist/DoubleLinkedList';
 import SingleLinkedList from './linkedlist/SingleLinkedList';
 import Queue from './queue/Queue';
 console.log('¡Proyecto TypeScript inicializado correctamente!'); 
 
 
 const linkedList = new SingleLinkedList<number>();
-
+/*
 linkedList.add(1);
 linkedList.add(2);
 linkedList.add(3);
@@ -26,3 +27,25 @@ linkedList.clear();
 
 console.log('--------------------------------LIMPIANDO....');
 linkedList.print();
+
+*/
+const doubleLinkedList = new DoubleLinkedList<number>();
+
+doubleLinkedList.add(1);
+doubleLinkedList.add(2);
+doubleLinkedList.add(3);
+
+
+doubleLinkedList.print();
+
+console.log('--------------------------------REMOVIENDO....');
+doubleLinkedList.remove(1);
+
+doubleLinkedList.print();
+
+console.log('--------------------------------TAMAÑO....');
+console.log(doubleLinkedList.getSize());
+
+console.log('--------------------------------LIMPIANDO....');
+doubleLinkedList.clear();
+doubleLinkedList.print();
--- src/linkedlist/DoubleLinkedList.ts
+++ src/linkedlist/DoubleLinkedList.ts
@@ -1,13 +1,17 @@
 import { Node } from './Node';
 
-class DoubleLinkedList<T> {
+
+
+export class DoubleLinkedList<T> {
     private head: Node<T> | null;
     private tail: Node<T> | null;
+    private prev: Node<T> | null;
     private size: number;
  
     constructor() {
         this.head = null;
         this.tail = null;
+        this.prev = null;
         this.size = 0;
     }
 
@@ -16,26 +20,39 @@ class DoubleLinkedList<T> {
         if (!this.head) {
             this.head = node;
             this.tail = node;
+            
         }
+        else {
+            this.tail!.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
+        this.size++;
     }
 
     remove(value: T) : void {
         if (this.head?.value === value) {
             this.head = this.head.next;
+            this.head!.prev = null;
             this.size--;
             if (!this.head) {
                 this.tail = null;
+                this.prev = null;
             }
             return;
         }
         
         let current = this.head;
         while (current?.next) {
+
             if (current.next.value === value) {
                 current.next = current.next.next;
+                current.next!.prev = current;
+
                 this.size--;
                 if (!current.next) {
                     this.tail = current;
+                    this.tail!.next = null;
                 }
                 return;
             }
@@ -51,5 +68,30 @@ class DoubleLinkedList<T> {
         }
         return false;
     }
-    
+
+    getSize() : number {
+        return this.size;
+    }
+
+    isEmpty() : boolean {
+        return this.size === 0;
+    }
+
+    clear() : void {
+        this.head = null;
+        this.tail = null;
+        this.prev = null;
+        this.size = 0;
+    }
+
+    print() : void {
+        let current = this.head;
+        while (current) {
+            console.log('prev: ',current?.prev?.value); 
+            console.log('current: ',current.value);
+            console.log('next: ',current?.next?.value); 
+            current = current.next;
+            console.log('--------------------------------');
+        }
+    }
 }
